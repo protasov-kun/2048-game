5f8b76e916f3cfc2cf97fcbd756e3b88c99dcb05
protas <protasov-kun@yandex.ru>
2024-03-26 18:43:10 +0300

    add deploy with ansible-galaxy, image version 1.0

--- run_terraform_output.sh
+++ run_terraform_output.sh
@@ -7,7 +7,7 @@ ip_address=$(terraform output | grep external_ip_address_game01 | cut -d' ' -f3 
 temp_file=$(mktemp)
 
 # Заменяем IP-адрес в файле hosts
-awk -v ip="$ip_address" '/game01/ {sub(/ansible_host=[0-9.]*/, "ansible_host=" ip)} 1' hosts > "$temp_file"
+awk -v ip="$ip_address" '/178.154.202.149/ {sub(/ansible_host=[0-9.]*/, "ansible_host=" ip)} 1' hosts > "$temp_file"
 
 # Перемещаем временный файл на место исходного
 mv "$temp_file" hosts
